50cd3b21c56b142ee4d9217efb2f0461992950c1
danielgis <daniel030891@gmail.com>
2023-06-02 18:29:21 -0500

    creacion de archivos de configuracion

--- widgets/CartoMaintenanceWgt/setting/Setting.js
+++ widgets/CartoMaintenanceWgt/setting/Setting.js
@@ -7,12 +7,28 @@ define(['dojo/_base/declare', 'jimu/BaseWidgetSetting'], function (declare, Base
       this.setConfig(this.config);
     },
     setConfig: function setConfig(config) {
-      this.textNode.value = config.serviceUrl;
+      this.text1Node.value = config.reasignacionUrl;
+      this.text2Node.value = config.acumulacionUrl;
+      this.text3Node.value = config.subdivisionUrl;
+      this.text4Node.value = config.inactivacionUrl;
+      this.text5Node.value = config.applicationListUrl;
+      this.text6Node.value = config.landsByApplicationUrl;
+      this.text7Node.value = config.observationUrl;
+      this.text8Node.value = config.resultsByApplication;
+      this.text9Node.value = config.updateStatusApplication;
     },
     getConfig: function getConfig() {
       // WAB will get config object through this method
       return {
-        serviceUrl: this.textNode.value
+        reasignacionUrl: this.text1Node.value,
+        acumulacionUrl: this.text2Node.value,
+        subdivisionUrl: this.text3Node.value,
+        inactivacionUrl: this.text4Node.value,
+        applicationListUrl: this.text5Node.value,
+        landsByApplicationUrl: this.text6Node.value,
+        observationUrl: this.text7Node.value,
+        resultsByApplication: this.text8Node.value,
+        updateStatusApplication: this.text9Node.value
       };
     }
   });
